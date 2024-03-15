9dc1f186641bd7e3ed11209bc509f6367fb74fbb
Omar <omarsegura76300@gmail.com>
2024-03-15 16:15:05 +0100

    tailwind

--- views/view-home.php
+++ views/view-home.php
@@ -9,20 +9,25 @@
 </head>
 
 <body>
-<nav class="shadow-2xl">
-    <div class="flex items-center justify-between px-4 py-2">
-        <!-- Texte à gauche -->
-        <span class="text-2xl text-green-500 font-bold mr-2">Wizpe<span class="text-blue-500">e</span></span>
-        
-        
-        <div class="flex items-center">
-            <!-- Liens de la navbar (Connexion, Inscription, etc.) -->
-            <a href="../controllers/controller-signin.php"><button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-2">Connexion</button></a>
-            <a href="controller-signup.php"><button class="bg-gray-300 hover:bg-gray-400 text-blue-500 font-bold py-2 px-4 rounded ml-4 m-2">Inscription</button></a>
-            <a href="../controllers/controller-profil.php"><img src="../assets/image/ImageParDéfaut.jpg" alt="Image de profil par défaut" class="w-8 h-8 rounded-full mr-2 m-3 hover:bg-gray-700"></a>
+    <nav class="shadow-2xl">
+        <div class="flex items-center justify-between px-4 py-2">
+            <!-- Texte à gauche -->
+            <span class="text-2xl text-green-500 font-bold mr-2">Wizpe<span class="text-blue-500">e</span></span>
+
+
+            <div class="flex items-center">
+                <!-- Liens de la navbar (Connexion, Inscription, etc.) -->
+                <a href="../controllers/controller-signin.php"><button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-2">Connexion</button></a>
+                <a href="controller-signup.php"><button class="bg-gray-300 hover:bg-gray-400 text-blue-500 font-bold py-2 px-4 rounded ml-4 m-2">Inscription</button></a>
+                <a href="../controllers/controller-profil.php"><img src="../assets/image/ImageParDéfaut.jpg" alt="Image de profil par défaut" class="w-8 h-8 rounded-full mr-2 m-3 hover:bg-gray-700"></a>
+            </div>
         </div>
+    </nav>
+
+    <!-- En dessous de la navbar je met une image avec une div à gauche de l'image avec du texte dessus -->
+    <div class="w-10/12 h-1/2 mx-auto flex justify-center items-center">
+        <img src="../assets/image/IA.jpg" alt="Image d'IA">
     </div>
-</nav>
 
 
 </body>
