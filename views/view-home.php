efc88c94539522dcfa151b37ef5f1bd29f080ae3
Omar <omarsegura76300@gmail.com>
2024-03-16 18:37:21 +0100

    accueil presque fini

--- views/view-home.php
+++ views/view-home.php
@@ -8,7 +8,7 @@
     <title>Accueil</title>
 </head>
 
-<body>
+<body class="bg-gray-300">
     <nav class="shadow-2xl">
         <div class="flex items-center justify-between px-4 py-2">
             <!-- Texte à gauche -->
@@ -32,7 +32,21 @@
         </div>
     </section>
 
-
+    <section class="py-24">
+        <div class="container mx-auto">
+            <div class="flex justify-around">
+                <div class="w-1/3 p-8 bg-white shadow-lg rounded-md mr-4">
+                    <h2 class="text-3xl font-bold mb-4 font-mono">Qu'est-ce que l'IA ?</h2>
+                    <p class="text-lg font-mono">L'intelligence artificielle (IA) est une discipline de l'informatique qui vise à créer des machines capables d'imiter certaines capacités intellectuelles humaines.</p>
+                    <button type="button" class="bg-blue-500 hover:bg-blue-600 text-white font-bold rounded mt-4 p-2 font-mono">Je me forme</button>
+                </div>
+                <div class="w-1/3 p-8 bg-white shadow-lg rounded-md ml-4">
+                    <h2 class="text-3xl font-bold mb-4 font-mono">Qu'est-ce que le Cloud ?</h2>
+                    <p class="text-lg font-mono">Le Cloud, ou "informatique en nuage", désigne l'utilisation de serveurs distants pour stocker, gérer et traiter des données sur Internet, plutôt que sur un ordinateur local ou un serveur physique.</p>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
 
